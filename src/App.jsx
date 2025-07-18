81911815c345f2aef4650720f3bcb511ce271e80
Ali <moinali5599@gmail.com>
2025-07-18 18:56:00 -0400

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import gsap from 'gsap'
 import {ScrollTrigger, SplitText} from 'gsap/all'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-    <div className='flex-center h-screen'>
-      <h1 className='text-3xl text-indigo-400'>Text large</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'>
+
+      </div>
+    </main>
   )
 }
 
